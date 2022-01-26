dd357130c9e4b89afb1f92b908dc0edd776905d4
EduardoConsul <eduardoconsul@tengroup.com>
2022-01-26 19:09:52 +0000

    first commit

---
+++ PriceResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PriceResponse
+    {
+        public decimal Tax { get; set; }
+
+        public string InsurerName { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
